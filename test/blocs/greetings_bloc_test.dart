c2891796643f664f59975e41b20e46582c258f10
Victor Kamau <victormungai97@gmail.com>
2022-06-30 03:47:03 +0300

    Created tests for controller and bloc

--- test/blocs/greetings_bloc_test.dart
+++ test/blocs/greetings_bloc_test.dart
@@ -1,16 +1,19 @@
+/// Define tests for the Greetings Bloc
+/// // for bloc test, you
+/// 1. provide the bloc/cubit in `build`,
+/// 2. add the event/call function in `act` and
+/// 3. list the expected state(s) in the order of being emitted in `expect`
+
 import 'package:bloc_test/bloc_test.dart';
 import 'package:test/test.dart';
 import 'package:widgetbook_challenge/blocs/bloc.dart';
 import 'package:widgetbook_challenge/controllers/controller.dart';
 
 void main() {
-  // Let us create a collections of test for the greetings functionality
-  // for bloc test, you
-  // 1. provide the bloc/cubit in `build`,
-  // 2. add the event/call function in `act` and
-  // 3. list the expected state(s) in the order of being emitted in `expect`
+  // Let us create a collections of test for the greetings bloc
+
   group(
-    'Greetings Functionality Tests. It shall include tests for the BLoC, controller and API used for requesting a greeting',
+    'Greetings BLoC Tests.',
     () {
       late GreetingsController greetingsController;
 
@@ -37,14 +40,6 @@ void main() {
       );
 
       // b) Test when no/empty name is provided for greeting
-      test(
-        "The controller should return an error message asking for the user's name when a null string is provided",
-        () async {
-          final result = await greetingsController.getGreeting(name: null);
-          expect(result, "Please enter the user's name");
-        },
-      );
-
       blocTest<GreetingsBloc, GreetingsState>(
         "The bloc should emit an error message asking for the user's name when a null string is provided",
         build: () => greetingsBloc,
@@ -55,14 +50,6 @@ void main() {
           const GreetingsLoadingState(),
           const GreetingsErrorState(message: "Please enter the user's name"),
         ],
-      );
-
-      test(
-        "The controller should return an error message asking for the user's name when an empty string is provided",
-        () async {
-          final result = await greetingsController.getGreeting(name: '');
-          expect(result, "Please enter the user's name");
-        },
       );
 
       blocTest<GreetingsBloc, GreetingsState>(
@@ -78,14 +65,6 @@ void main() {
       );
 
       // c) Test when provided name contains non - whitespace / letter character
-      test(
-        'The controller should return an error message when name contains a character that is not a letter or whitespace',
-        () async {
-          final result = await greetingsController.getGreeting(name: '1');
-          expect(result, 'Only letters allowed');
-        },
-      );
-
       blocTest<GreetingsBloc, GreetingsState>(
         'The bloc should emit an error message when name contains a character that is not a letter or whitespace',
         build: () => greetingsBloc,
@@ -99,16 +78,6 @@ void main() {
       );
 
       // d) Test when an error that is not [UnexpectedException] occurs
-      test(
-        'The controller should return an error message saying something went wrong when an exception that is not `UnexpectedException` is thrown',
-        () async {
-          final result = await greetingsController.getGreeting(
-              name: 'hi', throwError: true);
-
-          expect(result, 'Something went wrong. Contact support');
-        },
-      );
-
       blocTest<GreetingsBloc, GreetingsState>(
         'The bloc should emit an error message saying error getting response when an exception that is not `UnexpectedException` is thrown',
         build: () => greetingsBloc,
@@ -122,21 +91,6 @@ void main() {
       );
 
       // e) Test when proper name (with only letters & whitespace) is provided
-      test(
-        'The controller should return a success message containing hello name or an error message of `Unexpected Error` when a correct name containing only letters and/or whitespace is given',
-        () async {
-          final result =
-              await greetingsController.getGreeting(name: 'WidgetBook');
-          expect(
-            result,
-            isIn(<String>[
-              'Hello WidgetBook',
-              'Unexpected Error has occurred. Try again later'
-            ]),
-          );
-        },
-      );
-
       test(
         'The bloc should emit a success message containing hello name or an error message of `Unexpected Error` when a correct name containing only letters and/or whitespace is given',
         () async {
